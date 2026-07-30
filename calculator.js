a34aab00ce7542d08a977175d6070b90dc455063
odinwalker <266474446+odinwalker@users.noreply.github.com>
2026-07-30 23:55:23 +0800

    思路整理

---
+++ calculator.js
@@ -0,0 +1,29 @@
+//function add(a, b) {
+//   return a + b;
+// }
+
+// function subtract(a, b) {
+//   return a - b;
+// }
+
+// function multiply(a, b) {
+//   return a * b;
+// }
+
+// function divide(a, b) {
+//   return a / b;
+// }
+
+// function operate (op, a, b ) {
+//   a = Number(a);
+//   b = Number(b);
+//   if (op === '+') return a + b;
+//   if (op === '-') return a - b;
+//   if (op === '&#215') return a * b;
+//   if (op === '\u00F7') return b === 0 ? "Nice try!" : a / b; 
+// }
+
+const nums = document.querySelectorAll('.number')
+const operator = document.querySelectorAll('.operate')
+const decimalPoint = document.querySelector('.dot')
+
